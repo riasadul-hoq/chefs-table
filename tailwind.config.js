4d5d016f015591a82919c2595c85afb66111a2eb
Riasadul Hoq <shaon777@gmail.com>
2024-06-18 18:08:18 +0100

    Daisy UI, Google Fonts Setup & Header added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "default-color": "#150b2b",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
