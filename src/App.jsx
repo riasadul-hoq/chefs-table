5e13f7527c229df7b53406da9676c0430ae41c5b
Riasadul Hoq <shaon777@gmail.com>
2024-06-20 17:25:43 +0100

    Recipe Section Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Banner from "./components/Banner/Banner";
 import Header from "./components/Header/Header";
+import Recipe from "./components/Recipe/Recipe";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header></Header>
       <div className="container mx-auto mt-10">
         <Banner></Banner>
+        <Recipe></Recipe>
       </div>
     </>
   );
---
+++ src/components/Recipe/Recipe.jsx
@@ -0,0 +1,16 @@
+const Recipe = () => {
+  return (
+    <div>
+      <div className="my-10 text-center">
+        <h1 className="text-4xl font-semibold text-default-color mb-8">
+          Our Recipes
+        </h1>
+        <p className="text-base font-normal text-slate-500">
+          We use the fresh produce to create each recipe.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Recipe;
